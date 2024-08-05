dcb923b8eba7ba2202898d9f2b1d4ac3fae7e6c0
“Kyle <kylemboomer@gmail.com>
2024-08-05 10:25:04 -0500

    Set Up API Calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,34 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+const BASE_URL = 'http://localhost:3001/api/v1/urls'
+
+export const getUrls = async () => {
+  try {
+    const res = await fetch(`${BASE_URL}`)
+    if (!res.ok) {
+      throw new Error('Failed to fetch your URLs.')
+    }
+    const data = await res.json()
+    return DataTransfer
+  } catch (error) {
+    console.error('Failed to fetch data.', error.message)
+  }
+
+}
+
+export const postURL = async (card) => {
+  try {
+    const res = await fetch(`${BASE_URL}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application.json'
+      },
+      body: JSON.stringify(card)
+    })
+    if (!res.ok) {
+      throw new Error('Failed to post your data.')
+    }
+    const newCard = await res.json()
+    return newCard
+  } catch (error) {
+    console.error('Error:', error.message)
+  }
 }
